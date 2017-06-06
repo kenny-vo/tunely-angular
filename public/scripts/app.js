70728a2a6eed209e9049e620cd61f7b5a274f4b1
Kenneth Vo <kennyvo91@gmail.com>
2017-06-06 14:59:42 -0500

    completed sprint 2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,27 +11,40 @@ angular
   // the second argument is a function that defines the capacities
   // of the controller.
 
-function AlbumsIndexController () {
-  var vm = this;
-  vm.newAlbum = {};
+  AlbumsIndexController.$inject = ['$http'];
+  function AlbumsIndexController (  $http  ) {
+    var vm = this;
+    vm.newAlbum = {};
 
-  vm.newAlbum = {
-      name: 'Viva Hate',
-      artistName: 'Morrissey'
-  };
+    vm.newAlbum = {
+        name: 'Viva Hate',
+        artistName: 'Morrissey'
+    };
 
-  vm.albums = [
-    {
-      name: 'Coming Home',
-      artistName: 'Leon Bridges'
-    },
-    {
-      name: 'Are We There',
-      artistName: 'Sharon Van Etten'
-    },
-    {
-      name: 'The Queen is Dead',
-      artistName: 'The Smiths'
-    }
-  ];
+    vm.albums =[];
+
+  $http({
+    method: 'GET',
+    url: 'http://localhost:3000/api/albums'
+  }).then(function successCallback(response) {
+    vm.albums = response.data;
+    // console.log('response for all albums:', response);
+  }, function errorCallback(response) {
+    console.log('There was an error getting the data', response);
+  });
+
+  vm.createAlbum = function () {
+    $http({
+      method: 'POST',
+      url: 'http://localhost:3000/api/albums',
+      data: vm.newAlbum
+    }).then(function successCallback(response) {
+      vm.newAlbum = {};
+      vm.albums.push(response.data);
+      console.log('response for all albums:', response);
+    }, function errorCallback(response) {
+      console.log('There was an error getting the data', response);
+    });
+  }
+
 }
